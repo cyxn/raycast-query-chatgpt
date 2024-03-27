7a0c7599b1466adc1c4433512af9ef6b5d4d2256
cyxn <nick.sukhov@eze.tech>
2024-03-27 10:01:00 +0100

    wip

--- src/compose-applescript.ts
+++ src/compose-applescript.ts
@@ -24,15 +24,32 @@ export function composeApplescript({ browserName, prompt, gptUrl }: ComposeApple
   const completeUrl = composeUrlWithRandomId(gptUrl);
   const tabJavascript = getTabJavascript(prompt);
   return `
+set urlExists to false
+set urlToCompare to "${gptUrl}"
+set urlToUse to "${completeUrl}"
 tell application "${browserName}"
-    open location "${completeUrl}"
+    repeat with w in (every window)
+        repeat with t in (every tab of w)
+            if URL of t is urlToCompare then
+                set urlExists to true
+                exit repeat
+            end if
+        end repeat
+        if urlExists then
+            set urlToUse to "${gptUrl}"
+            exit repeat
+        end if
+    end repeat
+    if not urlExists then
+        open location urlToUse
+    end if
 end tell
 
 delay 2
 
 tell application "${browserName}"
     repeat with w in (every window)		
-        repeat with t in (every tab whose URL equal "${completeUrl}") of w
+        repeat with t in (every tab whose URL equal urlToUse) of w
           tell t
             return ${runJS(browserName, tabJavascript)}
           end tell
